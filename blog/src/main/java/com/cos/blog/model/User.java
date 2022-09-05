2dc30015c7fd566f2f39cc30153ad63c781f4dfc
gukwanglim <100823895+gukwanglim@users.noreply.github.com>
2022-09-06 02:09:33 +0900

    회원가입 테스트

--- blog/src/main/java/com/cos/blog/model/User.java
+++ blog/src/main/java/com/cos/blog/model/User.java
@@ -1,15 +1,19 @@
 package com.cos.blog.model;
 
-import java.security.Timestamp;
+
+import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,7 +24,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+//@DynamicInsert  -> insert 할 때, null인 필드를 제외해준다
 @Entity     // User class가 자동으로 데이터 베이스 테이블에 생성
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
 	@Id        // Primary key
@@ -36,8 +42,9 @@ public class User {
 	@Column(nullable = false, length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'")
-	private String role;    // Enum을 사용하는게 좋다(도메인을 사용하기 위해서) -> admin, user, manager를 분간할 수 있게 된다.
+//	@ColumnDefault("user")
+	@Enumerated(EnumType.STRING)
+	private RoleType role;    // Enum을 사용하는게 좋다(도메인을 사용하기 위해서) -> admin, user, manager를 분간할 수 있게 된다.
 	
 	@CreatedDate   // 시간이 자동으로 입력
 	private Timestamp createDate;
