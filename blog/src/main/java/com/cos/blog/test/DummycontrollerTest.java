901b199ed4660a136bf91eddc33ebee3144085d3
gukwanglim <100823895+gukwanglim@users.noreply.github.com>
2022-09-08 05:01:10 +0900

    user update

--- blog/src/main/java/com/cos/blog/test/DummycontrollerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummycontrollerTest.java
@@ -1,11 +1,20 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -18,6 +27,52 @@ public class DummycontrollerTest {
 	
 	@Autowired    // 의존성 주입(DI)
 	private UserRepositroy UserRepositroy;
+	
+	// email, password를 수정한다면
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		// 이 방식을 사용하면 null 값이 들어가게 된다. (@Transactional이 존재하지 않았다.)
+//		requestUser.setId(id);
+//		requestUser.setUsername("test");		
+//		UserRepositroy.save(requestUser);
+		
+		// save를 이용하여 정보를 수정하기 위해서는 이런 방식으로 코드를 작성해야한다. (@Transactional이 존재하지 않았다.)
+//		User user = UserRepositroy.findById(id).orElseThrow(()->{
+//			return new IllegalArgumentException("수정에 실패하였습니다.");
+//		});
+//		user.setPassword(requestUser.getPassword());
+//		user.setEmail(requestUser.getEmail());
+//		
+//		UserRepositroy.save(user);
+		
+		// 더티 체킹
+		// save를 사용하지 않으면 이렇게 작성해야한다. (@Transactional 사용)
+		User user = UserRepositroy.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		return null;
+	}
+	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/user")
+	public List<User> list() {
+		return UserRepositroy.findAll();
+	}
+	
+	// 한 페이지 당, 두 건의 데이터를 return
+	@GetMapping("/dummy/user/page")
+	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+		List<User> users = UserRepositroy.findAll(pageable).getContent();
+		return users;
+	}
 	
 	// {id} 주소로 파라메타를 전달 받을 수 있음
 	// http://localhost:8000/blog/dummy/user/3 과 같이 들어가게 된다.
